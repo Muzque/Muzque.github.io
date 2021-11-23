fcf3d30cfae70f3e09f148e9cadacc45dc259907
DESKTOP-202107\zeros <zerosky1943@gmail.com>
2021-11-23 22:16:06 +0800

    feat(layout): add theme toggle button
    - Add theme toggle button

--- pages/index.js
+++ pages/index.js
@@ -3,11 +3,17 @@ import { Container, Box, Heading } from '@chakra-ui/react'
 const Page = () => {
     return (
         <Container>
-            <Box borderRadius="lg" bg="red" p={3} align="center" mb={6}>
+            <Box
+                borderRadius="lg"
+                bg="red"
+                p={3}
+                align="center"
+                mb={6}
+            >
                 Hello, I&apos;m a full-stack developer based in Taiwan!
             </Box>
 
-            <Box display={{md:"flex"}}>
+            <Box display={{ md:"flex" }}>
                 <Box flexGrow={1}>
                     <Heading as="h2" variant="page-title">
                         Xuan-Di Lin
